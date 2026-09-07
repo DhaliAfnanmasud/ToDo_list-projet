95469cc01385679c959d63d3ac9987d541228731
Dhali Afnanmasud <afnanmasud.dhali@gmail.com>
2026-09-07 17:40:14 +0200

    développement de la fonction 'suprrimer'

--- to_do_list.py
+++ to_do_list.py
@@ -14,14 +14,23 @@ def menu():
 def ajouter_tache():
     nb_taches = int(input("\nCombien de tâches voulez-vous ajouter ? : " ))
     for _ in range(nb_taches):  
-        tache = input("Entrez votre tâche : ")
-        taches.append({"tache": tache, "status": "False"})
-        print(f"\nLa tache '{tache}' a été ajoutée à votre To Do List")
+        tache = input("\nEntrez votre tâche : ")
+        taches.append(tache)
+        print(f"La tache '{tache}' a été ajoutée à votre To Do List")
 
 def montrer_tache():
     print("\nVoici votre To Do List : ")
     for i, tache in enumerate(taches, start=1):
-        print(f"{i}. {tache['tache']} - {'Terminée' if tache['status'] == 'True' else 'En cours'}")
+        print(f"{i}. {tache}")
+
+
+def supprimer_tache():
+    montrer_tache()
+    numero_tache = int(input("\nEntrez le numéro de la tâche à supprimer : "))
+    if numero_tache > 0 and numero_tache <= len(taches):
+        tache_supprimer = taches.pop(numero_tache - 1)
+        print(f"La tache '{tache_supprimer}' a été supprimée de votre To Do List")
+
 
 def merci():  
     print("\nA bientôt dans votre To Do List !")
@@ -37,7 +46,7 @@ while True:
         case "2" :
             montrer_tache()
         case "3" :
-            pass
+            supprimer_tache()
         case "4" :
             merci()
             break
